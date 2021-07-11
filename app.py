0deff8de3a54c5f196e5ee5b40d853ee2abab979
Arpit Somani <arpit.somanias8@gmail.com>
2021-07-11 22:12:38 +0530

    Final Model

--- app.py
+++ app.py
@@ -2,3 +2,5 @@ from flask import Flask, render_template, request
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
+
+data = pd.read_csv('model.csv')
