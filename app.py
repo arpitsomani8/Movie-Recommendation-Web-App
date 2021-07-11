64b562483123a875dbdfb62e3c30331cb4bde3c1
Arpit Somani <arpit.somanias8@gmail.com>
2021-07-11 22:16:31 +0530

    CountVectorizer

--- app.py
+++ app.py
@@ -4,3 +4,6 @@ from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
 
 data = pd.read_csv('model.csv')
+
+vec = CountVectorizer()
+vec_matrix = vec.fit_transform(data['combined']
