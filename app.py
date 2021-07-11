279f85b057a9281784e28f22bc92439a992a9d28
Arpit Somani <arpit.somanias8@gmail.com>
2021-07-11 21:47:19 +0530

    Imports

---
+++ app.py
@@ -0,0 +1,4 @@
+from flask import Flask, render_template, request
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import pandas as pd
